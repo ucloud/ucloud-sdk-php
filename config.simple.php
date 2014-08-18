7ee3d2e044f4ffeab57d0506b0717dbbff5afe4c
ucloud-web <wentianle@ucloud.cn>
2014-08-19 01:02:33 +0800

    update config 去除默认填写的 公钥信息
    update config 去除默认填写的 公钥信息

--- config.simple.php
+++ config.simple.php
@@ -1,7 +1,7 @@
 <?php
 #配置公私钥"
-define("PUBLIC_KEY" ,"ucloudwentianle@ucloud.cn13595660461202471273");
-define("PRIVATE_KEY" ,"8a78e7bde21c6301d2fc35b56091b159dedbf2e9");
+define("PUBLIC_KEY" ,"");
+define("PRIVATE_KEY" ,"");
 
 #配置api地址'''
 #无外网IP主机使用(ucloud 云主机中)
